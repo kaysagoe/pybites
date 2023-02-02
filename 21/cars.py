716f6e0698d43b55c26e98c21958d661abbe1e40
Kanyinsola Sagoe <ksagoe@gmail.com>
2023-02-02 08:27:27 +0000

    complete byte 21

---
+++ 21/cars.py
@@ -0,0 +1,57 @@
+from typing import Dict, List
+from functools import reduce
+
+cars = {
+    "Ford": ["Falcon", "Focus", "Festiva", "Fairlane"],
+    "Holden": ["Commodore", "Captiva", "Barina", "Trailblazer"],
+    "Nissan": ["Maxima", "Pulsar", "350Z", "Navara"],
+    "Honda": ["Civic", "Accord", "Odyssey", "Jazz"],
+    "Jeep": ["Grand Cherokee", "Cherokee", "Trailhawk", "Trackhawk"],
+}
+DEFAULT_SEARCH = "trail"
+CarsType = Dict[str, List[str]]
+
+
+def get_all_jeeps(cars: CarsType = cars) -> str:
+    """
+    Retrieve the 'Jeep' models from the cars dict and join them by a
+    comma and space (', '). Leave the original ordering intact.
+    """
+    return ", ".join(cars["Jeep"])
+
+
+def get_first_model_each_manufacturer(cars: CarsType = cars) -> List[str]:
+    """
+    Loop through the cars dict filtering out the first model for each
+    manufacturer. Return the matching models in a list leaving the original
+    ordering intact.
+    """
+    return [cars[key][0] for key in cars.keys()]
+
+
+def get_all_matching_models(
+    cars: CarsType = cars, grep: str = DEFAULT_SEARCH
+) -> List[str]:
+    """
+    Return a list of all models containing the case insensitive
+    'grep' string which defaults to DEFAULT_SEARCH ('trail').
+    Sort the resulting sequence alphabetically
+    """
+    return [
+        car
+        for car in sorted(
+            reduce(lambda cum, current: cum + current, list(cars.values()))
+        )
+        if grep.lower() in car.lower()
+    ]
+
+
+def sort_car_models(cars: CarsType = cars) -> CarsType:
+    """
+    Loop through the cars dict returning a new dict with the
+    same keys and the values sorted alphabetically.
+    """
+    new_cars = dict()
+    for key in sorted(cars.keys()):
+        new_cars[key] = sorted(cars[key])
+    return new_cars
